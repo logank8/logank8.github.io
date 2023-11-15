7c6806edef423a7e406e9ba6d40e610cdc90fdbd
8bitlogan <lokeener@student.ubc.ca>
2023-11-14 17:55:06 -0800

    basename changed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Blog from './pages/blog';
 // add content to screens
 function App() {
   return (
-    <Router>
+    <Router basename="/personal-website">
       <Navbar />
       <Routes>
         <Route path='/' exact element={<Home />}></Route>
