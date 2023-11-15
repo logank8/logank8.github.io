ba663ab08e48d607c5bc23d081a5cfbbc976a9e5
8bitlogan <lokeener@student.ubc.ca>
2023-11-14 17:49:57 -0800

    routing workaround adding

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='logank8.github.io/' exact element={<Home />}></Route>
-        <Route path='logank8.github.io/blog' element={<Blog />} ></Route>
-        <Route path='logank8.github.io/projects' element={<Projects />} ></Route>
-        <Route path='logank8.github.io/contact' element={<Contact />} ></Route>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/blog' element={<Blog />} ></Route>
+        <Route path='/projects' element={<Projects />} ></Route>
+        <Route path='/contact' element={<Contact />} ></Route>
       </Routes>
     </Router>
   );
